5ec5461bde2be174e6fdb9e9f5b7fa6eeb74555b
park <ruden91@naver.com>
2017-08-12 19:08:46 +0900

    add vue framework

---
+++ resources/assets/js/App.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div id="app">
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      msg: 'vue project'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ resources/assets/js/components/Header.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+  <p>header</p>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
